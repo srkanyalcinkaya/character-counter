976b4f417fc26b2f38fa7abf278c5befae51a8c6
SerkanYalçınkaya <syalcinkaya895@gmail.com>
2024-02-27 00:53:24 +0300

    Update footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,8 +6,8 @@ export default function Footer() {
     return (
 
 
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-zinc-600">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <footer className="bg-white rounded-lg shadow m-4 dark:bg-black/60">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {year} <a href="https://github.com/srkanyalcinkaya" className="hover:underline">CharacterCheck™</a>. All Rights Reserved.
                 </span>
                 {/* <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
